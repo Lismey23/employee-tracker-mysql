ee24fd1f48e099c420605667d3ff0f761df86479
Lismey Plasencia <lismeyplasencia@gmail.com>
2021-03-16 18:57:46 -0400

    added inquirer cases

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const mysql = require('mysql');
 const inquire = require('inquirer')
+const consoleTable = require("console.table");
+const promisemysql = require("promise-mysql");
+const inquirer = require('inquirer');
 
-
+// Creating connection
 
 const connection = mysql.createConnection({
   host     : 'localhost',
@@ -11,16 +14,78 @@ const connection = mysql.createConnection({
   database : 'employee_db',
 });
 
-connection.connect((err)=>{
+// Starting connection 
+
+connection.connect((err) => {
 
     if(err) throw err;
-    console.log("connection stated ")
+    console.log("WELCOME TO YOUR EMPLOYEE TRACKER ");
+    startPrompt();
 });
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  });
+// Start Prompt
+
+function startPrompt() {
+
+    inquirer.prompt({
+        name: 'userchoice',
+        type:'list',
+        message: 'Main Menu',
+        choices: [
+        
+        'View all employees', 
+        'View all employees by role', 
+        'View all employees by department', 
+        'Add role', 
+        'Add employee', 
+        'Add department', 
+        'Update employee role', 
+        ]
+    })
+    .then((answer) => {
+        switch(answer.userchoice) {
+            case 'View all employees':
+                viewEmployees();
+                break;
+
+            case 'View all employees by role':
+                viewEmplByRole();
+                break;
+            
+            case 'View all employees by department':
+                viewEmpByDept();
+                break;
+
+            case 'Add role':
+                addRole();
+                break;
+
+            case 'Add department':
+                addDepartment();
+                break;
+
+            case 'Add employee':
+                 addEmployee();
+                break;
+
+            case 'Update employee role':
+                updateEmployeeRole();
+                break;
+                    
+
+        }
+    })
+    
 
 
+
+
+
+}
+
+
+
+/// Start prompts when connection is started
+
+//
 // connection.end();
